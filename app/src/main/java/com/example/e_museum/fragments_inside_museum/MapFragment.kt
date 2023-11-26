b3f01beeb464b3935ec537c92acc2355166b949c
Duc Anh <doducanh2411@gmail.com>
2023-11-26 23:42:29 +0700

    Change map but have bug

--- app/src/main/java/com/example/e_museum/fragments_inside_museum/MapFragment.kt
+++ app/src/main/java/com/example/e_museum/fragments_inside_museum/MapFragment.kt
@@ -1,13 +1,16 @@
 package com.example.e_museum.fragments_inside_museum
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import com.example.e_museum.R
 import com.example.e_museum.databinding.FragmentMapBinding
+
 
 class MapFragment : Fragment() {
 
@@ -15,6 +18,7 @@ class MapFragment : Fragment() {
 
     private val binding get() = _binding!!
 
+    @SuppressLint("ClickableViewAccessibility")
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -28,7 +32,29 @@ class MapFragment : Fragment() {
         (activity as AppCompatActivity).supportActionBar?.setDisplayHomeAsUpEnabled(true)
 
         val photoView = binding.photoView;
-        photoView.setImageResource(R.drawable.hcm_museum_map);
+        photoView.setImageResource(R.drawable.demomap);
+
+        val demoBut = binding.button2
+        demoBut.setOnClickListener { v ->
+            // Define the center point for zoom
+            val centerX = 150f
+            val centerY = 150f
+
+            // Define the desired scale factor
+            val scaleFactor = 2.0f // Change this value according to your requirement
+
+            // Zoom in with a focal point at (centerX, centerY)
+            photoView.setScale(scaleFactor, centerX, centerY, true)
+        }
+
+
+        val resetBut = binding.button3;
+        resetBut.setOnClickListener { v ->
+            // Đặt lại các thuộc tính về giá trị mặc định
+            photoView.scale = 1f
+            photoView.setScale(1f, 0f, 0f, true)
+        }
+
         return root
     }
 
