74dffed2f7afe6cd0a72161f3eb01439acdd753c
Nguyễn Đăng Dương <21020757@vnu.edu.vn>
2023-12-05 15:43:56 +0700

    fix search collection

--- app/src/main/java/com/example/e_museum/view_controller/fragments/fragments_inside_museum/CollectionsListFragment.kt
+++ app/src/main/java/com/example/e_museum/view_controller/fragments/fragments_inside_museum/CollectionsListFragment.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.widget.SearchView
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.DividerItemDecoration
@@ -12,22 +13,24 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.e_museum.R
 import com.example.e_museum.view_controller.activities.MainActivity
 import com.example.e_museum.adapters.CollectionsListAdapter
+import com.example.e_museum.adapters.NotificationsListAdapter
 import com.example.e_museum.data_fetching.models.Model
 import com.example.e_museum.databinding.FragmentCollectionsListBinding
+import com.example.e_museum.databinding.FragmentNotificationsBinding
 import com.example.e_museum.entities.Collection
 import com.example.e_museum.entities.Museum
+import com.example.e_museum.entities.Notification
 import com.example.e_museum.entities.Thing
 
 class CollectionsListFragment : Fragment() {
-    private lateinit var binding: FragmentCollectionsListBinding
+//    private lateinit var _binding: FragmentCollectionsListBinding? = null
     private lateinit var adapter: CollectionsListAdapter
+    private var _binding: FragmentCollectionsListBinding? = null
 
+    private val binding get() = _binding!!
     @SuppressLint("NotifyDataSetChanged")
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View {
-        binding = FragmentCollectionsListBinding.inflate(inflater, container, false)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
         val museum = activity?.intent?.getSerializableExtra("museum") as Museum
 
         val collections = ArrayList<Collection>()
@@ -59,6 +62,32 @@ class CollectionsListFragment : Fragment() {
                 adapter.notifyDataSetChanged()
             }
         }.start()
+
+        binding.collectionSearchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                adapter.filter.filter(query)
+                return true
+            }
+
+            override fun onQueryTextChange(newText: String?): Boolean {
+                adapter.filter.filter(newText)
+                return true
+            }
+        })
+    }
+
+
+    @SuppressLint("NotifyDataSetChanged")
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentCollectionsListBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
